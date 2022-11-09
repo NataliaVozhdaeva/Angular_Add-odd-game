bdd3f4b6774061847cbad2e428c3efe4d4aed20c
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-11-09 23:18:12 +0200

    create interface WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,20 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'Game-odd-even';
+  oddNumbers: number[] = [];
+  evenNumbers: number[] = [];
+
+  onIntervalFired(firedNumber: number) {
+    firedNumber % 2 === 0
+      ? this.evenNumbers.push(firedNumber)
+      : this.oddNumbers.push(firedNumber);
+  }
+  /* 
+  onIntervalStop(firedNumber: number) {
+    console.log(firedNumber);
+  } */
 }
