44b3f28b140c2dae7c2d6caa2bb0e9326ff1bbb8
NataliaVozhdaeva <LomzNat@yandex.ru>
2022-11-25 22:23:04 +0200

    auto create directive + @Host...

--- src/app/better-hightlight/better-highlight.directive.ts
+++ src/app/better-hightlight/better-highlight.directive.ts
@@ -4,6 +4,8 @@ import {
   OnInit,
   Renderer2,
   HostListener,
+  HostBinding,
+  Input,
 } from '@angular/core';
 
 @Directive({
@@ -12,36 +14,50 @@ import {
 export class BetterHighlightDirective implements OnInit {
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
 
+  @Input() defaultColor: string = 'transparent';
+  @Input() hightlightColor: string = 'blue';
+
+  //@HostBinding('style.backgroundColor') backgroundColor: string = 'transparent';
+  @HostBinding('style.backgroundColor') backgroundColor: string =
+    this.defaultColor;
   ngOnInit(): void {
     /*  this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'yellow'
-    ); */
+    ); это статично  */
+
+    this.backgroundColor = this.defaultColor;
   }
 
-  @HostListener('mouseenter') mouseover() {
-    this.renderer.setStyle(
+  @HostListener('mouseenter') mouseover(eventData: Event) {
+    /*  this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'yellow'
-    );
+    ); 
+    
+    это интерактивно но с renderer
+    */
+
+    /* this.backgroundColor = 'blue';
+    а это уже через @HostBinding без рендерера, но прибито однозначно и внутри  */
+
+    this.backgroundColor = this.hightlightColor;
   }
 
-  @HostListener('mouseleave') mouseleave() {
-    this.renderer.setStyle(
+  @HostListener('mouseleave') mouseleave(eventData: Event) {
+    /*   this.renderer.setStyle(
       this.elRef.nativeElement,
       'background-color',
       'transparent'
-    );
-  }
+    
+    это интерактивно но с renderer
+    ); */
+    /* 
+    this.backgroundColor = 'transparent';
+     а это уже через @HostBinding без рендерера, но прибито однозначно и внутри */
 
-  /*
-  @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(
-      this.elRef.nativeElement,
-      'background-color',
-      'yellow'
-    );
-  } */
+    this.backgroundColor = this.defaultColor;
+  }
 }
